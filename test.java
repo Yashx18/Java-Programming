5965fbac09db7c77d2237d3317591d46d3852cff
Ken <yash20004x17@gmail.com>
2024-12-05 23:17:22 +0530

    This repo is made to learn and practice the Java language so this is basic code being pushed to check if repo is working fine

--- test.java
+++
@@ -1,5 +0,0 @@
-public class test{
-    public static void main(String[] args) {
-        System.out.println("Hello Yash i know that you have different dreams than your entire bloddline you  want to be the best but yu still procrastinate and being lazy and that is an insult of the time you have been given by GOD. You do it by assuming that you have more which in reality isn't the case.");
-    }
-}
