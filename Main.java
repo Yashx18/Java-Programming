f0426ff0a21ad70322e7086e7f85e92cf7a296cd
ken <yash20004x17@gmail.com>
2025-08-12 10:50:09 +0000

    Completed tutorial till 12 topic

--- Main.java
+++ Main.java
@@ -59,32 +59,114 @@ public class Main {
         // }
 
 
-        String adjective1;
-        String noun1;
-        String adjective2;
-        String verb1;
-        String adjective3;
+        // String adjective1;
+        // String noun1;
+        // String adjective2;
+        // String verb1;
+        // String adjective3;
 
-        System.out.print("Enter adjective1 : ");
-        adjective1 = userInput.nextLine();
+        // System.out.print("Enter adjective1 : ");
+        // adjective1 = userInput.nextLine();
 
-        System.out.print("Enter noun1 : ");
-        noun1 = userInput.nextLine();
+        // System.out.print("Enter noun1 : ");
+        // noun1 = userInput.nextLine();
 
-        System.out.print("Enter adjective2 : ");
-        adjective2 = userInput.nextLine();
+        // System.out.print("Enter adjective2 : ");
+        // adjective2 = userInput.nextLine();
 
-        System.out.print("Enter verb1 : ");
-        verb1 = userInput.nextLine();
+        // System.out.print("Enter verb1 : ");
+        // verb1 = userInput.nextLine();
 
-        System.out.print("Enter adjective3 : ");
-        adjective3 = userInput.nextLine();
+        // System.out.print("Enter adjective3 : ");
+        // adjective3 = userInput.nextLine();
 
 
-        System.out.println("Today I went to a " + adjective1 + " zoo.");
-        System.out.println("In an exhibit, I saw a " + noun1 + ".");
-        System.out.println( noun1 + " was " + adjective2 + " and " + verb1 + " !");
-        System.out.println( "I was " + adjective3 + "!");
+        // System.out.println("Today I went to a " + adjective1 + " zoo.");
+        // System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        // System.out.println( noun1 + " was " + adjective2 + " and " + verb1 + " !");
+        // System.out.println( "I was " + adjective3 + "!");
+
+        // String item ;
+        // double price ;
+        // int quantity ;
+        // double total ;
+
+
+        // System.out.print("What item would you like to buy? : ");
+        // item = userInput.nextLine();
+
+        // System.out.print("What is the price for each? : ");
+        // price = userInput.nextDouble();
+        // userInput.nextLine();
+        
+        // System.out.print("How many would you like? : ");
+        // quantity = userInput.nextInt();
+        // userInput.nextLine();
+
+        // total = price * quantity;
+
+        // System.out.println("\nYou have bought "+ quantity + " " + item+ "/s.");
+        // System.out.println("You total is $"+ total + ".");
+
+        // double a;
+        // double b;
+        // double c;
+
+        // System.out.print("Enter length of Side A : ");
+        // a = userInput.nextDouble();
+
+        // System.out.print("Enter length of Side B : ");
+        // b = userInput.nextDouble();
+        
+        // c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+        // System.out.println("Hypotenuse of Side C is " + c);
+
+        // CODE TO FIND OUT AREA & CIRCUMFERENCE OF A CIRCLE & VOLUME OF SPHERE.
+
+        // double radius;
+        // double circumference;
+        // double area;
+        // double volume;
+
+        // System.out.print("Enter the Radius : ");
+        // radius = userInput.nextDouble();
+
+        // circumference = 2 * Math.PI * radius;
+        // area = Math.PI * Math.pow(radius, 2);
+        // volume = (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+        
+        // System.out.printf("Circumference of circle with %.1f is %.1f.\n", radius , circumference );
+        // System.out.printf("Area of circle with %.1f is %.1f.\n", radius , area );
+        // System.out.printf("Volume of Sphere with %.1f is %.1f.\n", radius , volume );
+
+
+        // COMPOUND INTEREST CALCULATOR 
+
+        double principal;
+        double rate;
+        int timesCompounded;
+        int years;
+        double amount;
+
+        System.out.print("Enter the Principal amount : ");
+        principal = userInput.nextDouble();
+        userInput.nextLine();
+
+        System.out.print("Enter the Rate of Interest(in %) : ");
+        rate = userInput.nextDouble() /100;
+        userInput.nextLine();
+
+        System.out.print("Enter the time of amount has to be compounded : ");
+        timesCompounded = userInput.nextInt();
+        userInput.nextLine();
+
+        System.out.print("Enter the years for amount is invested : ");
+        years = userInput.nextInt();
+        userInput.nextLine();
+
+        amount = principal * Math.pow(1 + rate / timesCompounded, timesCompounded * years);
+
+        System.out.printf("The Amount after %d years is $%,.2f.\n",years, amount);
 
         userInput.close();
     }
